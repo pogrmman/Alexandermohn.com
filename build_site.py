6d1858405d0db140ed7e97f6c451c35fb7ef83db
Alexander Mohn <pogrmman@gmail.com>
2021-05-22 17:28:06 -0500

    Prepare to walk articles and pages for rendering

--- build_site.py
+++ build_site.py
@@ -1,9 +1,10 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
 import pathlib
+import markdown
 import settings
 
-def make_jinja_params(filename):
+def make_jinja_params(filepath):
     jinja_params = {}
     f = pathlib.Path(filename)
     if f.is_file():
@@ -23,27 +24,42 @@ class Renderer(object):
             loader = FileSystemLoader(template_path),
             autoescape = select_autoescape(["html","xml"])
             )
-        for key, value in jinja_globals:
-            self.jinja_env.globals[key] = value
+        for key in jinja_globals:
+            self.jinja_env.globals[key] = jinja_globals[key]
 
-    def render_article(filename, template_name = None):
+    def render_article(self, filepath, template_name = None):
         if template_name:
             self.article_template = self.jinja_env.get_template(template_name)
         if self.article_template:
-            return article_template.render(make_jinja_params(filename))
+            return self.article_template.render(article = make_jinja_params(filepath))
         else:
             raise "A template to build articles with has not been specified!"
 
-    def render_page(filename, template_name = None):
+    def render_page(self, filepath, template_name = None):
         if template_name:
             self.page_template = self.jinja_env.get_template(template_name)
         if self.page_template:
-            return page_template.render(make_jinja_params(filename))
+            return self.page_template.render(page = make_jinja_params(filepath))
         else:
             raise "A template to build pages with has not been specified!"
+
+def walk_dir(path):
+    files = []
+    directories = []
+    for child in path.iterdir():
+        if child.is_dir():
+            directories.append(child)
+        elif child.is_file():
+            files.append(child)
+    for directory in directories:
+        files_in_dir = walk_dir(directory)
+        if files_in_dir:
+            files.append(files_in_dir)
+    return files
 
 # Create a jinja environment to render the files
 jinja_renderer = Renderer("templates", {"SITENAME": settings.SITENAME,
                                         "SITEURL": settings.SITEURL})
 
-        
+article_path = pathlib.Path("content/articles")
+article_files = walk_dir(article_path)
